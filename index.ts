197e5d9073f31453ed1406de3013f6023c2eff09
Andrew Webb <androbwebb@gmail.com>
2023-04-21 15:19:46 +0100

    Fix building index

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import * as fs from 'fs';
 import * as yargs from 'yargs';
 import v1Cli from './versions/v1';
--- shared/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.NEWEST_VERSION = exports.SupportedVersions = void 0;
-var SupportedVersions;
-(function (SupportedVersions) {
-    SupportedVersions["v1"] = "v1";
-})(SupportedVersions = exports.SupportedVersions || (exports.SupportedVersions = {}));
-exports.NEWEST_VERSION = SupportedVersions.v1;

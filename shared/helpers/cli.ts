36f66a37956497fd444a000eef6eaf4c84d05705
Andrew Webb <androbwebb@gmail.com>
2023-04-28 17:25:14 +0100

    Remove loggin

--- shared/helpers/cli.ts
+++ shared/helpers/cli.ts
@@ -1,11 +1,9 @@
 export const run = (main: () => Promise<void>): void => {
     main()
         .then(() => {
-            console.log('🍊 Done!')
             process.exit(0);
         })
         .catch((err) => {
-            console.log('🍊 Error!', err)
             process.exit(1);
         });
 };

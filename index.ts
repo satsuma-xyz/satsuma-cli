de58b6b3a2afd3cb5c4853954c2d5861077efa2b
Andrew Webb <androbwebb@gmail.com>
2023-05-10 11:01:52 +0100

    More console

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@
 import * as child_process from "child_process";
 import path from "path";
 
+console.log("running ts-node dir:", __dirname, process.argv.slice(2).join(" "));
 const location = path.resolve(__dirname, "run.ts");
 console.log("running ts-node", location, process.argv.slice(2).join(" "));
 
-child_process.execSync(`npx ts-node ${location} ` + process.argv.slice(2).join(" "));
+child_process.execSync(`npx --yes ts-node ${location} ` + process.argv.slice(2).join(" "));

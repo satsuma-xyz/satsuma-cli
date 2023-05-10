f114711cf959c16bf20e3148081d84fdf8d41786
Andrew Webb <androbwebb@gmail.com>
2023-05-10 10:54:20 +0100

    Correctly call run.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 // Just call `npx ts-node run.ts` with all the args
 
 import * as child_process from "child_process";
+import path from "path";
 
-child_process.execSync("npx ts-node run.ts " + process.argv.slice(2).join(" "));
+const location = path.resolve(process.cwd(), "run.ts");
+
+child_process.execSync(`npx ts-node ${location} ` + process.argv.slice(2).join(" "));
